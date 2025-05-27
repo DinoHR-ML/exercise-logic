2d628138a1fed784fe525de92360321aa0a4fdba
Henrique <henrique.hoppen@gmail.com>
2025-05-27 17:07:15 -0300

    Idade maior ou menor
    Vê a sua idade, e fala se você é de maior ou de menor.
    Mesmo que o outro, porém em java (arquivo.js

---
+++ idades.js
@@ -0,0 +1,10 @@
+// Ler a idade do usurário
+// Se a idade for maior ou igual a 18, escrever que é maior
+//Senão, escrever que é de menor
+
+let idade = prompt("Quantos anos você tem?\n")
+if(idade>=18){
+    alert("Você é de maior!")
+} else if(idade<=17){
+    alert("Você é de menor!")
+}
