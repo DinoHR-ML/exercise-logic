a5a4ff581c77127cd0daac7e36035a6d33c73f25
Henrique <henrique.hoppen@gmail.com>
2025-05-28 17:07:28 -0300

    HTML Combinadinhos | Testes exercícios java script

---
+++ SITE-00/01-script.js
@@ -0,0 +1,10 @@
+let senha
+
+senha = prompt("Escreva sua senha:")
+if(senha == "abracadabra"){
+    alert("PIROQUINHAS VOADORAS CAINDO NA SUA BOCA")
+}else if(senha == "meu pau"){
+    alert("Sua senha é muito pequena")
+}else{
+    alert("Que senha bosta")
+}
---
+++ SITE-00/02-script.js
@@ -0,0 +1,7 @@
+let idade
+idade = prompt("Qual a sua idade?")
+if(idade >=16){
+    alert("OPA! Você é elegível para votar!")
+} else{
+    alert("Desculpa mano, mas o destino do país está nas mãos de outras pessoas")
+}
